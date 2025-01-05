d28c480624e9a912df0828931e1d4bba59e5c19c
Denis <me@email>
2025-01-05 23:52:41 +0200

    feat: add new files

---
+++ christmas-shop/src/home.js
@@ -0,0 +1,89 @@
+import { renderModal } from "./items.js";
+/*
+    SLIDER
+*/
+
+const sliderContainer = document.querySelector(".slider-main");
+const sliderLeftBtn = document.querySelector("#slider-left-btn");
+const sliderRightBtn = document.querySelector("#slider-right-btn");
+
+let sliderElementPos = 0;
+
+const sliderShift = function () {
+    // Calculate the number of shifts based on the screen size
+    const shiftNum = window.innerWidth >= 768 ? 3 : 6;
+    const shiftValue = (sliderContainer.scrollWidth - sliderContainer.offsetWidth) / shiftNum; 
+    const shiftArr = [shiftValue, shiftNum];
+    return shiftArr;
+}
+
+// Slider scroll (direction arg: -1 is right direction, +1 is left direction)
+const sliderScroll = function (direction, value) {
+    sliderElementPos += (direction * value[0]);
+
+    // Disable slider buttons at the end and begining
+    if ( (Math.abs(sliderElementPos) / value[0]) === value[1] ) {
+        sliderRightBtn.setAttribute("disabled", "");
+    } else {
+        sliderRightBtn.removeAttribute("disabled");
+    }
+    if ( Math.abs(sliderElementPos) < value[0] ) {
+        sliderLeftBtn.setAttribute("disabled", "");
+    } else {
+        sliderLeftBtn.removeAttribute("disabled");
+    }
+    sliderContainer.style.transition = `transform ${value[0]}ms`;
+    sliderContainer.style.transform = `translateX(${sliderElementPos}px)`;
+}
+
+// Slider Events
+sliderRightBtn.onclick = () => {
+    sliderScroll(-1, sliderShift());
+}
+sliderLeftBtn.onclick = () => {
+    sliderScroll(1, sliderShift());
+}
+
+window.addEventListener("resize", () => {
+    if (sliderElementPos !== 0) {
+        sliderContainer.style.transform = `translateX(0px)`;
+        sliderLeftBtn.setAttribute("disabled", "");
+        sliderRightBtn.removeAttribute("disabled");
+        sliderElementPos = 0;
+    }
+})
+
+// CTA Timer
+
+// Set the date to the next New Year
+const year = new Date().getFullYear();
+const countDownDate = new Date(`Jan 1, ${year+1} 0:00:00`).getTime();
+const daysElement = document.getElementById("timer-days");
+const hoursElement = document.getElementById("timer-hours");
+const minutesElement = document.getElementById("timer-minutes");
+const secondsElement = document.getElementById("timer-seconds");
+
+var x = setInterval(function() {
+
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+    
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  // Output the result
+  daysElement.innerHTML = days;
+  hoursElement.innerHTML = hours;
+  minutesElement.innerHTML = minutes;
+  secondsElement.innerHTML = seconds; 
+
+  if (distance < 0) {
+    clearInterval(x);
+    daysElement.innerHTML = 0;
+    hoursElement.innerHTML = 0;
+    minutesElement.innerHTML = 0;
+    secondsElement.innerHTML = 0; 
+  }
+}, 1000);
---
+++ christmas-shop/src/items.js
@@ -0,0 +1,235 @@
+import data from './gifts.json' with {type: "json"};
+
+function getRandomNum(max, limit) {
+    const randomNumbers = new Set();
+    while (randomNumbers.size < limit) {
+        const randomNumber = Math.floor(Math.random() * max); 
+        randomNumbers.add(randomNumber); 
+    }
+    return [...randomNumbers];
+}
+
+function prepareData (data, limit, sort, name) {
+    let dataPrepared = [];
+    const dataLength = data.length;
+    let count = 0;
+    if (!limit) {
+        limit = dataLength;
+    }
+
+    switch(sort) {
+        case "random":
+            const randomNum = getRandomNum(dataLength, limit);
+            for (let i = 0; i < limit; i++ ) {
+                dataPrepared.push(data[randomNum[i]]);
+            }
+            break;
+        case "work":
+            count = 0;
+
+            for (let i = 0; i < dataLength; i++ ) {
+                if (data[i].category === 'For Work') {  
+                    count++;
+                    if (count <= limit) {
+                        dataPrepared.push(data[i]);
+                    } else {
+                        break;
+                    }
+                }
+            }
+            break;
+        case "health":
+            count = 0;
+
+            for (let i = 0; i < dataLength; i++ ) {
+                if (data[i].category === 'For Health') {  
+                    count++;
+                    if (count <= limit) {
+                        dataPrepared.push(data[i]);
+                    } else {
+                        break;
+                    }
+                }
+            }
+            break;
+        case "harmony":
+            count = 0;
+
+            for (let i = 0; i < dataLength; i++ ) {
+                if (data[i].category === 'For Harmony') {  
+                    count++;
+                    if (count <= limit) {
+                        dataPrepared.push(data[i]);
+                    } else {
+                        break;
+                    }
+                }
+            }
+            break;
+            case "modal":
+  
+                for (let i = 0; i < dataLength; i++ ) {
+                    if (data[i].name === name) {  
+                            dataPrepared.push(data[i]);
+                            break;
+                    }
+                }
+                break;
+        default:
+            for (let i = 0; i < dataLength; i++ ) {
+                    dataPrepared.push(data[i]);
+            }
+    }
+
+    return dataPrepared;
+}
+
+function prepareHTML(data, page) {
+    let path = "img";
+    if (page !== 'home') {
+        path = "../img";
+    }
+    const splitText = data.category.split(" ");
+    const category = splitText[1].toLowerCase();
+
+    const htmlTemplate = 
+    `
+        <div class="gifts-card-img">
+            <img src="${path}/gifts-items/gift-for-${category}.png" alt="gift picture" >
+        </div>
+        <div class="gifts-card-text gifts-card-text-${category}">
+            <h4>${data.category}</h4>
+            <h3>${data.name}</h3>
+        </div>
+    `;
+
+    return htmlTemplate;
+}
+
+function htmlModal(data, page) {
+    let path = "img";
+    if (page !== 'home') {
+        path = "../img";
+    }
+    const splitText = data.category.split(" ");
+    const category = splitText[1].toLowerCase();
+    const snowflakeImg = `<img src="${path}/gifts-items/snowflake-modal.svg" class="modal-snowflake">`;
+    const snowflakeImgEmpty = `<img src="${path}/gifts-items/snowflake-modal.svg" class="modal-snowflake-empty">`;
+    const htmlTemplate = 
+    `   
+        <div class="gifts-modal-item">
+            <div class="gifts-card-img">
+            <span class="closeBtn" id="modal-close-btn">
+                <svg width="40" height="40" viewBox="0 0 40 40" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" xmlns="http://www.w3.org/2000/svg">
+                <path d="M30 10L10 30"></path>
+                <path d="M10 10L30 30"></path>
+                </svg>
+            </span>
+            <img src="${path}/gifts-items/gift-for-${category}.png" alt="gift picture" >
+            </div>
+            <div class="gifts-card-text gifts-card-text-${category}">
+                <h4>${data.category}</h4>
+                <h3>${data.name}</h3>
+                <p>${data.description}</p>
+            </div>
+            <div class="gifts-modal-powers-container">
+                <h4>Adds superpowers to:</h4>
+                <div>
+                    <div class="gifts-modal-powers">
+                        <p>Live</p>
+                        <p>${data.superpowers.live}</p>
+                        <span>
+                            ${snowflakeImg.repeat(data.superpowers.live / 100)}
+                            ${snowflakeImgEmpty.repeat(5 - (data.superpowers.live / 100))}
+                        </span>
+                    </div>
+                    <div class="gifts-modal-powers">
+                        <p>Create</p>
+                        <p>${data.superpowers.create}</p>
+                        <span>
+                            ${snowflakeImg.repeat(data.superpowers.create / 100)}
+                            ${snowflakeImgEmpty.repeat(5 - (data.superpowers.create / 100))}
+                        </span>         
+                    </div>
+                    <div class="gifts-modal-powers">
+                        <p>Love</p>
+                        <p>${data.superpowers.love}</p>
+                        <span>
+                            ${snowflakeImg.repeat(data.superpowers.love / 100)}
+                            ${snowflakeImgEmpty.repeat(5 - (data.superpowers.love / 100))}
+                        </span>                    
+                    </div>
+                    <div class="gifts-modal-powers">
+                        <p>Dream</p>
+                        <p>${data.superpowers.dream}</p>
+                        <span>
+                            ${snowflakeImg.repeat(data.superpowers.dream / 100)}
+                            ${snowflakeImgEmpty.repeat(5 - (data.superpowers.dream / 100))}
+                        </span>                    
+                    </div>
+                </div>
+            </div>
+        </div>
+    `;
+
+    return htmlTemplate;
+}
+
+
+export function renderItems(page, sort, limit) {
+    const preparedData = prepareData(data, limit, sort);
+    // console.log(preparedData);
+
+    const itemsContainer = document.querySelector(".gifts-cards");
+
+    for (let i = 0; i < preparedData.length; i++) {
+        const newItem = document.createElement("div");
+        newItem.classList.add("gifts-cards-item");
+        newItem.innerHTML = prepareHTML(preparedData[i], page);
+        itemsContainer.append(newItem);
+
+        newItem.onclick = () => {
+            renderModal(preparedData[i].name, page);
+        }
+    }    
+
+}
+
+export function renderModal(item, page) {
+    const preparedData = prepareData(data, 1, 'modal', item);
+    const main = document.querySelector(".main-container");
+
+    const newModal = document.createElement("div");
+    newModal.classList.add("gifts-modal");
+    newModal.innerHTML = htmlModal(preparedData[0], page);
+    main.append(newModal);
+
+    // remove scrollbar
+    htmlDocument.setAttribute("style", "overflow: hidden");
+
+    const modalItem = document.querySelector(".gifts-modal-item");
+    
+    let modalClose = true;
+    modalItem.onmouseover = () => {
+        modalClose = false;
+    }
+    modalItem.onmouseout = () => {
+        modalClose = true;
+    }
+    newModal.onclick = () => {
+        if (modalClose === true) {
+            newModal.remove();
+            htmlDocument.removeAttribute("style", "overflow: hidden");
+        } 
+    }
+
+    // Close Button 
+    let closeBtn = document.getElementById("modal-close-btn");
+    
+    closeBtn.onclick = () => {
+        newModal.remove();
+        htmlDocument.removeAttribute("style", "overflow: hidden");
+
+    }
+
+}
